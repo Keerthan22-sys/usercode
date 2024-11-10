1e876b9153a5dc87798811f33b9fd22e47891dcf
Keerthan <keerthangowdas222@gmail.com>
2024-11-10 13:08:49 +0530

    task 11 - retrieved, displayed, play and remove favorites

--- music-player/src/app/favorites/favorites.component.ts
+++ music-player/src/app/favorites/favorites.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MusicPlayerState } from '../helpers/music-player-state';
+import { MusicService } from '../services/music.service';
+import { tracks } from '../helpers/tracks';
 
 @Component({
   selector: 'app-favorites',
@@ -7,19 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesComponent implements OnInit {
   // Task 11: Add required variables here
-
+  state = MusicPlayerState;
+  favTracks: any = [];
   constructor(
   // Task 11: Add private declarations here
-
+private musicService: MusicService
   ) {}
 
   ngOnInit(): void {
     // Task 11: Retreive favorite tracks here
-
+    tracks.forEach((track) => {
+      if (track.isFavorite === true) {
+        this.favTracks.push(track);
+      }
+    });
   }
 
   // Task 11: Add playTrack() method here
-  
+  playTrack(file: any) {
+    this.musicService.playNewFile(file);
+  }
   // Task 11: Add removeFromFavorites() method here
-      
+  removeFromFavorites(index: any) {
+    tracks[tracks.indexOf(this.favTracks[index])].isFavorite = false;
+    this.favTracks.splice(index, 1);
+  }
 }
