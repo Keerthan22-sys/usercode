819845ac897d25dd7b182c3d4316fd01c5cca5b5
Keerthan <keerthangowdas222@gmail.com>
2024-11-10 12:02:34 +0530

    task3 - implement an audio playback control

--- music-player/src/app/services/music.service.ts
+++ music-player/src/app/services/music.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import { MusicPlayerState } from '../helpers/music-player-state';
 @Injectable({
   providedIn: 'root',
 })
 export class MusicService {
   // Task 3: Declate required variables here
+state = MusicPlayerState;
+audio = new Audio();
 
   constructor() {
       // Task 4: Add solution here
@@ -12,12 +14,31 @@ export class MusicService {
   }
 
   // Task 3: Add resumse() method here
-
+resume() {
+  this.audio.play();
+  this.state.isPlaying = true;
+}
   // Task 3: Add pause() method here
-
+pause() {
+  this.audio.pause();
+  this.state.isPlaying = false;
+}
   // Task 3: Add goToTime() method here
 
+  goToTime(seconds: number){
+    this.audio.currentTime = seconds;
+    this.state.currentTime = this.audio.currentTime;
+  }
   // Task 3: Add changePlaybackSpeed() method here
+changePlaybackSpeed(speed: number) {
+  this.audio.playbackRate = speed;
+}
 
+playNewFile (file: any) {
+  this.state.isPlaying = true;
+  this.state.activeTrack = file;
+  this.audio.src = file.url;
+  this.audio.play();
+}
 
 }
