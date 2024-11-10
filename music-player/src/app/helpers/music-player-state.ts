e687868a9b6d4f741cc09ea8c7a813b9c56088d5
Keerthan <keerthangowdas222@gmail.com>
2024-11-10 11:54:40 +0530

    added state objects

--- music-player/src/app/helpers/music-player-state.ts
+++ music-player/src/app/helpers/music-player-state.ts
@@ -1,1 +1,14 @@
 // Task 2: Add the state object here
+export const MusicPlayerState = {
+    duration: 0,
+    currentTime: 0,
+    isPlaying: false,
+    playbackRate: 0,
+    activeTrack: {
+        url: '',
+        name: '',
+        artist: '',
+        img: '',
+        isFavotire: false
+    }
+}
