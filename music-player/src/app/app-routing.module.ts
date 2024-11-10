aa9e31fc2e4c87f028f564910102b2d0ba31a633
Keerthan <keerthangowdas222@gmail.com>
2024-11-10 11:47:22 +0530

    task 1 - added routes

--- music-player/src/app/app-routing.module.ts
+++ music-player/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // Task 1: Import components here
+import { TracksComponent } from './tracks/tracks.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 const routes: Routes = [
 // Task 1: Add routes here
+{path: "", component: TracksComponent},
+{path: "favorites", component: FavoritesComponent}
 
 ];
 
