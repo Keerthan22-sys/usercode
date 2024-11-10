114ac8836dd9a582cbc03ceafea96a576d1daa5f
Keerthan <keerthangowdas222@gmail.com>
2024-11-10 12:33:16 +0530

    task 6 -handle the progress bar and seek functionalities

--- music-player/src/app/app.component.ts
+++ music-player/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import * as moment from 'moment';
+import { MusicPlayerState } from './helpers/music-player-state';
+import { tracks } from './helpers/tracks';
+import { MusicService } from './services/music.service';
 
 @Component({
   selector: 'app-root',
@@ -8,22 +11,27 @@ import * as moment from 'moment';
 })
 export class AppComponent {
   // Task 6: Declare required constants here
-  
+ state = MusicPlayerState;
+ readonly tracks = tracks; 
   // Task 9: Declare required constants here
   
 
   constructor(
     // Task 6: Declare private variable here
-
+public musicService: MusicService
   ) {}
 
   ngOnInit(): void {
   }
   
   // Task 6: Add goToTime() method here
-
+goToTime(change: any) {
+  this.musicService.goToTime(change.value);
+}
   // Task 6: Add getFormattedTime() method here
-
+getFormattedTime(timeInSeconds: number) {
+  return moment.utc(timeInSeconds * 1000).format('mm:ss');
+}
   // Task 7: Add pause() method here
 
   // Task 7: Add resume() method here
